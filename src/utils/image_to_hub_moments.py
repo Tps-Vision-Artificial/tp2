a70f6c96238e115d9ced94177ee939a48073f3f1
ignam <ignacio.mendez@ing.austral.edu.ar>
2021-09-12 20:03:04 -0300

    Create 'descriptores.csv'

--- src/utils/image_to_hub_moments.py
+++ src/utils/image_to_hub_moments.py
@@ -1,10 +1,7 @@
-# import cv2 as cv
-# import numpy as np
-# import math
+import cv2
+import numpy as np
+import math
 import csv
-#
-# from src.detection_main import apply_color_convertion, threshold, denoise, get_contours
-
 
 # def generate_hu_moments_file():
 #
@@ -28,25 +25,45 @@ import csv
 #         writer.writerow(row)
 
 def read_supervision():
+    descriptores = []
     with open('../dataset/supervision.csv') as file:
         reader = csv.reader(file)
         for row in reader:
-            print(row[0])
+            element = [row[0], get_hub_moments('../dataset/imagenes/'+row[0])]
+            descriptores.append(element)
+
+    with open('../dataset/descriptores.csv','w', newline='') as file:
+        writer = csv.writer(file,delimiter =',')
+        writer.writerows(descriptores)
+
+def get_hub_moments(file):
+     image = cv2.imread(file)
+     gray = apply_color_convertion(image,cv2.COLOR_BGR2GRAY)
+     _, threshold_image = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+     denoised_image = denoise(frame=threshold_image, method=cv2.MORPH_ELLIPSE, radius=5)
+     countours = get_contours(frame=denoised_image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+     moments = cv2.moments(countours)  # momentos de inercia
+     # Calculate Hu Moments
+     huMoments = cv2.HuMoments(moments)  # momentos de Hu
+
+     # Log scale hu moments
+     for i in range(0, 7):
+         huMoments[i] = -1 * math.copysign(1.0, huMoments[i]) * math.log10(
+             abs(huMoments[i]))  # Mapeo para agrandar la escala.
+     return huMoments
+
+def denoise(frame, method, radius):
+    kernel = cv2.getStructuringElement(method, (radius, radius))
+    opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    return closing
+
+def get_contours(frame, mode, method):
+    contours, hierarchy = cv2.findContours(frame, mode, method)
+    return contours
+
+def apply_color_convertion(frame, color):
+    return cv2.cvtColor(frame, color)
 
 read_supervision()
-# def get_hub_moments(file):
-#     image = cv.imread(file)
-#     gray = apply_color_convertion(frame=image, color=cv.COLOR_BGR2GRAY)
-#     _, threshold_image = threshold(frame=gray, slider_max=255, trackbar_value=50)
-#     denoised_image = denoise(frame=threshold_image, method=cv.MORPH_ELLIPSE, radius=5)
-#     countours = get_contours(frame=denoised_image, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
-#
-#     moments = cv.moments(countours)  # momentos de inercia
-#     # Calculate Hu Moments
-#     huMoments = cv.HuMoments(moments)  # momentos de Hu
-#
-#     # Log scale hu moments
-#     for i in range(0, 7):
-#         huMoments[i] = -1 * math.copysign(1.0, huMoments[i]) * math.log10(
-#             abs(huMoments[i]))  # Mapeo para agrandar la escala.
-#     return huMoments
