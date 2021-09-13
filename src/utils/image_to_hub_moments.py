39288c362e20a1a96cef505c2d9c60a3b5edb6ae
ignam <ignacio.mendez@ing.austral.edu.ar>
2021-09-13 20:05:11 -0300

    Create 'descriptores.csv'

--- src/utils/image_to_hub_moments.py
+++ src/utils/image_to_hub_moments.py
@@ -1,8 +1,8 @@
 import os
 
-import cv2
+import cv2 as cv
 import numpy as np
-import math
+from math import log10, copysign
 import csv
 
 
@@ -44,36 +44,36 @@ def read_supervision():
 
 
 def get_hub_moments(file):
-    image = cv2.imread(file)
-    gray = apply_color_convertion(image, cv2.COLOR_BGR2GRAY)
-    _, threshold_image = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    denoised_image = denoise(frame=threshold_image, method=cv2.MORPH_ELLIPSE, radius=5)
-    countours = get_contours(frame=denoised_image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    image = cv.imread(file)
+    gray = apply_color_convertion(image, cv.COLOR_BGR2GRAY)
+    _, threshold_image = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+    denoised_image = denoise(frame=threshold_image, method=cv.MORPH_ELLIPSE, radius=5)
+    countours = get_contours(frame=denoised_image, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
 
-    moments = cv2.moments(countours[1])  # momentos de inercia
+    moments = cv.moments(countours[1])  # momentos de inercia
     # Calculate Hu Moments
-    huMoments = cv2.HuMoments(moments)  # momentos de Hu
+    huMoments = cv.HuMoments(moments)  # momentos de Hu
 
     # Log scale hu moments
     for i in range(0, 7):
-        huMoments[i] = -1 * math.copysign(1.0, huMoments[i]) * math.log10(abs(huMoments[i]))
+        huMoments[i] = -1 * copysign(1.0, huMoments[i]) * log10(abs(huMoments[i]))
     return huMoments
 
 
 def denoise(frame, method, radius):
-    kernel = cv2.getStructuringElement(method, (radius, radius))
-    opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    kernel = cv.getStructuringElement(method, (radius, radius))
+    opening = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
+    closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
     return closing
 
 
 def get_contours(frame, mode, method):
-    contours, hierarchy = cv2.findContours(frame, mode, method)
+    contours, hierarchy = cv.findContours(frame, mode, method)
     return contours
 
 
 def apply_color_convertion(frame, color):
-    return cv2.cvtColor(frame, color)
+    return cv.cvtColor(frame, color)
 
 
 read_supervision()
